90885131b6c4c77c791203dfe6b00fa0de03ab6d
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-02-22 21:06:37 +0300

    implement signup page tests

--- src/test/java/ru/snsin/cakefactory/client/BrowserClient.java
+++ src/test/java/ru/snsin/cakefactory/client/BrowserClient.java
@@ -1,6 +1,10 @@
 package ru.snsin.cakefactory.client;
 
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.web.htmlunit.LocalHostWebClient;
@@ -26,7 +30,33 @@ public class BrowserClient {
         this.currentPage = this.mvcInitClient.getPage("/");
     }
 
+    public void goToSignupPage() throws IOException {
+        this.currentPage = this.mvcInitClient.getPage("/signup");
+    }
+
     public String pageText() {
         return this.currentPage.asText();
+    }
+
+    public void fillInUserCredentials(String login, String password) {
+        setValue("#user-email", login);
+        setValue("#user-password", password);
+    }
+
+    public void fillInAddress(String line1, String line2, String postcode) {
+        setValue("#addressLine1", line1);
+        setValue("#addressLine2", line2);
+        setValue("#postCode", postcode);
+    }
+
+    public void clickToSignUpButton() throws IOException {
+        final HtmlElement signUpButton = this.currentPage.querySelector("#sign-up");
+        this.currentPage = signUpButton.click();
+
+    }
+
+    private void setValue(String selector, String value) {
+        final HtmlInput input = this.currentPage.querySelector(selector);
+        input.setValueAttribute(value);
     }
 }
