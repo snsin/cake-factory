4a9628d47430d2cd77c63b0607c405ba49c70df8
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-02-28 14:28:59 +0300

    add constraints to user and address, save user and address on sign-up

--- src/main/java/ru/snsin/cakefactory/users/SignUpController.java
+++ src/main/java/ru/snsin/cakefactory/users/SignUpController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import java.util.Collections;
 
 @Controller
@@ -15,15 +16,25 @@ import java.util.Collections;
 @Slf4j
 public class SignUpController {
 
+    private final UserService userService;
+    private final AddressService addressService;
+
+    public SignUpController(UserService userService, AddressService addressService) {
+        this.userService = userService;
+        this.addressService = addressService;
+    }
+
     @GetMapping
     public ModelAndView signup() {
         return new ModelAndView("signup", Collections.singletonMap("login?", true));
     }
 
     @PostMapping
-    public RedirectView signUpNewUser(User user, Address address) {
+    public RedirectView signUpNewUser(@Valid User user, @Valid Address address) {
         log.info("New user signed up with email = {}", user.getEmail());
         log.info("and address = {}", address);
+        userService.save(user);
+        addressService.save(address, user.getEmail());
         return new RedirectView("/");
     }
 }
