ad8e469b4765eed74a79fa6a07def42aae42a506
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-02-28 13:07:46 +0300

    update JpaAddressServiceImpl due to requirements

--- src/main/java/ru/snsin/cakefactory/users/persistence/AddressRepository.java
+++ src/main/java/ru/snsin/cakefactory/users/persistence/AddressRepository.java
@@ -2,5 +2,8 @@ package ru.snsin.cakefactory.users.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
+    Optional<AddressEntity> findByUserId(String userId);
 }
