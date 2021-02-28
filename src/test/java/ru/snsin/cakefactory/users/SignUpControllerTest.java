7396b57d9265f31357e8aa2cdbc5447f65bc82a5
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-02-28 14:14:52 +0300

    extend SignUpControllerTest

--- src/test/java/ru/snsin/cakefactory/users/SignUpControllerTest.java
+++ src/test/java/ru/snsin/cakefactory/users/SignUpControllerTest.java
@@ -1,12 +1,17 @@
 package ru.snsin.cakefactory.users;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SignUpController.class)
@@ -15,10 +20,41 @@ class SignUpControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    AddressService addressService;
+
     @Test
     void signUpPageShouldExist() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/signup"))
+        mockMvc.perform(get("/signup"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void userShouldNotSignUpWhenParametersInvalid() throws Exception {
+        mockMvc.perform(post("/signup"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void userAndAddressShouldStoreOnSignup() throws Exception {
+        User user = new User("test@example.com", "pass");
+        Address address = new Address("al1", "al2", "code");
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("email", user.getEmail());
+        requestParams.add("password", user.getPassword());
+        requestParams.add("addressLine1", address.getAddressLine1());
+        requestParams.add("addressLine2", address.getAddressLine2());
+        requestParams.add("postcode", address.getPostcode());
+
+        mockMvc.perform(post("/signup").params(requestParams))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+
+        Mockito.verify(userService).save(user);
+        Mockito.verify(addressService).save(address, user.getEmail());
     }
 
 }
