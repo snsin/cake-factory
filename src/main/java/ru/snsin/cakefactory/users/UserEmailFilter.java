9ca9120769dcc2978f34425b573d505b8a2ad36d
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-05-07 23:18:26 +0300

    implement facebook login tests

--- src/main/java/ru/snsin/cakefactory/users/UserEmailFilter.java
+++ src/main/java/ru/snsin/cakefactory/users/UserEmailFilter.java
@@ -3,7 +3,6 @@ package ru.snsin.cakefactory.users;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
