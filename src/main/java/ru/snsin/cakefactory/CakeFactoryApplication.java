51c44caa0a29c4dfa115d07fa5cea6468f24a1bf
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-07-18 21:53:27 +0300

    add PayPal configurations

--- src/main/java/ru/snsin/cakefactory/CakeFactoryApplication.java
+++ src/main/java/ru/snsin/cakefactory/CakeFactoryApplication.java
@@ -2,8 +2,11 @@ package ru.snsin.cakefactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.snsin.cakefactory.order.PayPalConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PayPalConfig.class)
 public class CakeFactoryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ru/snsin/cakefactory/order/PayPalConfig.java
@@ -0,0 +1,31 @@
+package ru.snsin.cakefactory.order;
+
+import com.paypal.core.PayPalEnvironment;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import com.paypal.core.PayPalHttpClient;
+
+@ConfigurationProperties(prefix = "pay-pal")
+@ConstructorBinding
+@AllArgsConstructor
+public class PayPalConfig {
+    private final String clientId;
+    private final String secret;
+
+    @Bean
+    @Profile("!prod")
+    PayPalHttpClient sandboxClient() {
+        PayPalEnvironment sandboxEnv = new PayPalEnvironment.Sandbox(clientId, secret);
+        return new PayPalHttpClient(sandboxEnv);
+    }
+
+    @Bean
+    @Profile("prod")
+    PayPalHttpClient liveClient() {
+        PayPalEnvironment liveEnv = new PayPalEnvironment.Live(clientId, secret);
+        return new PayPalHttpClient(liveEnv);
+    }
+}
